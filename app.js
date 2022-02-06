f94dff3ba62d0676223595befa0096f6a6b00cff
Codehouzem <codehouseinc@gmail.com>
2022-02-06 19:49:23 +0100

    installed cors

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middleware =jsonServer.defaults();
 const port = process.env.PORT || 3000;
+const cors = require('cors');
 
 server.use(middleware);
 server.use(router);
+server.use(cors());
+
 
 server.listen(port);
